862f31914dc9bc63bec946c9649838fd45505f1a
Emiya Shiro <tuquochung123@gmail.com>
2023-05-30 12:13:37 +0700

    Build frontend again.

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,8 +5,9 @@
 export const environment = {
 	production: false,
 	appName: "Udagram",
-	//apiHost: "http://acac932585c89488eaae4d20906b2a6e-1327118789.us-east-1.elb.amazonaws.com:8080/api/v0",
-	apiHost: "test",
+	apiHost:
+		"http://acac932585c89488eaae4d20906b2a6e-1327118789.us-east-1.elb.amazonaws.com:8080/api/v0",
+	//apiHost: "test",
 };
 
 /*
